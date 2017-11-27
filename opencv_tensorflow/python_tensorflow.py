8b2b378646a84c2ee01e43b1a8529e1449ac3e74
Terry <dageda1991@163.com>
2017-11-27 15:18:44 +0800

    Update python_tensorflow.py

--- opencv_tensorflow/python_tensorflow.py
+++ opencv_tensorflow/python_tensorflow.py
@@ -1,16 +1,43 @@
-# read input image using opencv
+# inference using opencv
 import numpy as np
 import cv2
+
+# read and convert the image to the format that similar to mnist image
 x_image = cv2.imread('test8.png', cv2.IMREAD_GRAYSCALE)
 x_image = cv2.resize(x_image, dsize=(28, 28))
-
 x_image = 255 - x_image
-x_image = x_image / 255. #normalization
-x_image = np.reshape(x_image, [-1, 28, 28, 1]) #according to the network input data size
+x_image = x_image / 255.
+x_image = x_image.astype(np.float32)
+
+# read model and inference
+inputBlob = cv2.dnn.blobFromImage(x_image)
+
+net = cv2.dnn.readNetFromTensorflow('trained_model/frozen_model.pb')
+net.setInput(inputBlob)
+result = net.forward()
+
+print(result)
+print(np.argmax(result, 1))
 
 
-# read from .pb file
+################################################################################################
+
+# inference using tensorflow
 import tensorflow as tf
+import numpy as np
+import cv2
+
+# read and convert the image to the format that similar to mnist image
+x_image = cv2.imread('test8.png', cv2.IMREAD_GRAYSCALE)
+x_image = cv2.resize(x_image, dsize=(28, 28))
+x_image = 255 - x_image
+x_image = x_image / 255.
+x_image = x_image.astype(np.float32)
+
+
+# read model and inference
+x_image = np.reshape(x_image, [-1, 28, 28, 1])
+
 with open('trained_model/frozen_model.pb', 'rb') as f:
     out_graph_def = tf.GraphDef()
     out_graph_def.ParseFromString(f.read())
@@ -24,4 +51,5 @@ with open('trained_model/frozen_model.pb', 'rb') as f:
         sess.run(tf.global_variables_initializer())
         x_image_out = sess.run(prediction, feed_dict={data: x_image})
 
+        print(x_image_out)
         print(np.argmax(x_image_out, 1))
