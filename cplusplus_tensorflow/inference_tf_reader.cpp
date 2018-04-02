9edb65cea3bfaed44e3cb049841cd72d32496540
Terry <dageda1991@163.com>
2018-04-02 16:33:04 +0800

    Update inference_tf_reader.cpp

--- cplusplus_tensorflow/inference_tf_reader.cpp
+++ cplusplus_tensorflow/inference_tf_reader.cpp
@@ -6,7 +6,6 @@
 #define PLATFORM_WINDOWS   // 指定使用tensorflow/core/platform/windows/cpu_info.h
 
 #include <iostream>
-#include <opencv2/opencv.hpp>   // 使用opencv读取图片
 #include "tensorflow/core/public/session.h"
 #include "tensorflow/core/platform/env.h"
 #include "tensorflow/cc/ops/standard_ops.h"  // 为了使用ops
