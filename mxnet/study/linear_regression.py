1514ac45b16b846bd962a3065fec844cbac25245
TerryBryant <dageda1991@163.com>
2018-04-05 22:03:04 +0800

    update mxnet_study

--- mxnet/study/linear_regression.py
+++ mxnet/study/linear_regression.py
@@ -13,3 +13,22 @@ y = true_w[0] * X[:, 0] + true_w[1] * X[:, 1] + true_b
 y += 0.01 * nd.random_normal(shape=y.shape)
 
 print(X[0], y[0])
+
+import matplotlib.pyplot as plt
+plt.scatter(X[:, 1].asnumpy(), y.asnumpy())
+plt.show()
+
+import random
+batch_size = 10
+
+def data_iter():
+	# 产生一个随机索引
+	idx = list(range(num_examples))
+	random.shuffle(idx)
+	for i in range(0, num_examples, batch_size):
+		j = nd.array(idx[i:min(i+batch_size, num_examples)])
+		yield nd.take(X, j), nd.take(y, j)
+
+for data, label in data_iter():
+	print(data, label)
+	break
---
+++ mxnet/study/study_gluon1.py
@@ -0,0 +1,27 @@
+from mxnet import nd
+from mxnet.gluon import nn
+
+# net = nn.Sequential()
+# with net.name_scope():
+# 	net.add(nn.Dense(256, activation="relu"))
+# 	net.add(nn.Dense(10))
+#
+# print(net)
+
+class MLP(nn.Block):
+	def __init__(self, **kwargs):
+		super(MLP, self).__init__(**kwargs)
+		with self.name_scope():
+			self.dense0 = nn.Dense(256)
+			self.dense1 = nn.Dense(10)
+
+	def forward(self, x):
+		return self.dense1(nd.relu(self.dense0(x)))
+
+net2 = MLP()
+print(net2)
+
+net2.initialize()
+x = nd.random_uniform(shape=(4, 20))
+y = net2(x)
+print(y)
