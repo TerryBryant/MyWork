f85f4b265353bc56c866379fb02f5cc9e327ab6e
Terry <dageda1991@163.com>
2018-07-04 21:52:19 +0800

    Update linear_regression.py

--- mxnet/study/linear_regression.py
+++ mxnet/study/linear_regression.py
@@ -1,34 +1,49 @@
-import mxnet.ndarray as nd
-import mxnet.autograd as ag
+from matplotlib import pyplot as plt
+from mxnet import autograd, nd
+import random
 
-# 创建数据集
 num_inputs = 2
 num_examples = 1000
-
 true_w = [2, -3.4]
 true_b = 4.2
 
-X = nd.random_normal(shape=(num_examples, num_inputs))
-y = true_w[0] * X[:, 0] + true_w[1] * X[:, 1] + true_b
-y += 0.01 * nd.random_normal(shape=y.shape)
+features = nd.random.normal(scale=1, shape=(num_examples, num_inputs))
+labels = true_w[0] * features[:, 0] + true_w[1] * features[:, 1] + true_b
+labels += nd.random.normal(scale=0.001, shape=labels.shape)
 
-print(X[0], y[0])
+# plt.rcParams['figure.figsize'] = (3.5, 2.5)
+# plt.scatter(features[:, 0].asnumpy(), labels.asnumpy(), 1)
+# plt.show()
 
-import matplotlib.pyplot as plt
-plt.scatter(X[:, 1].asnumpy(), y.asnumpy())
-plt.show()
 
-import random
 batch_size = 10
 
-def data_iter():
-	# 产生一个随机索引
-	idx = list(range(num_examples))
-	random.shuffle(idx)
-	for i in range(0, num_examples, batch_size):
-		j = nd.array(idx[i:min(i+batch_size, num_examples)])
-		yield nd.take(X, j), nd.take(y, j)
+# data loader
+def data_iter(batch_size, features, labels):
+    num_examples = len(features)   # len returns the max of the dimensions
+    indices = list(range(num_examples))
 
-for data, label in data_iter():
-	print(data, label)
-	break
+    # randomly read the samples
+    random.shuffle(indices)
+    for i in range(0, num_examples, batch_size):
+        j = nd.array(indices[i: min(i + batch_size, num_examples)])
+        yield features.take(j), labels.take(j)
+
+
+# for X, y in data_iter(batch_size, features, labels):
+#     print(X, y)
+#     break
+
+
+# initial model params
+w = nd.random.normal(scale=0.01, shape=(num_inputs, 1))
+b = nd.zeros(shape=(1,))
+params = [w, b]
+
+# make gradient
+for param in params:
+    param.attach_grad()
+
+# define model
+def linreg(X, w, b):
+    return nd.dot(X, w) + b
